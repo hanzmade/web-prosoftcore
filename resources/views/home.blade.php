84828091bf901c154a78050ebd7a86556b34499d
Hanif Ramadhan <h4nif.ramadhan@gmail.com>
2020-07-10 22:21:49 +0700

    fixing data table 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,6 +27,9 @@
   <link href="/vendor/aos/aos.css" rel="stylesheet">
   <link rel="stylesheet" href="/css/jquery.dataTables.min.css">
   <link rel="stylesheet" href="/css/buttons.dataTables.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.2.7/css/rowReorder.dataTables.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.dataTables.min.css">
 
   <!-- Template Main CSS File -->
   <link href="/css/style.css" rel="stylesheet">
@@ -70,43 +73,6 @@
       align: center;
     }
     
-    @media 
-    only screen and (max-width: 760px),
-    (min-device-width: 768px) and (max-device-width: 1024px)  {
-
-      /* Force table to not be like tables anymore */
-      table, thead, tbody, th, td, tr { 
-        display: block; 
-      }
-      
-      /* Hide table headers (but not display: none;, for accessibility) */
-      thead tr { 
-        position: absolute;
-        top: -9999px;
-        left: -9999px;
-      }
-      
-      tr { border: 1px solid #ccc; }
-      
-      td { 
-        /* Behave  like a "row" */
-        border: none;
-        border-bottom: 1px solid #eee; 
-        position: relative;
-        padding-left: 50%; 
-      }
-      
-      td:before { 
-        /* Now like a table header */
-        position: absolute;
-        /* Top/left values mimic padding */
-        top: 6px;
-        left: 6px;
-        width: 45%; 
-        padding-right: 10px; 
-        white-space: nowrap;
-      }
-    }
   </style>
 
 </head>
@@ -1191,6 +1157,9 @@
         <script type="text/javascript">
               $('#example').DataTable( {
                   responsive: true,
+                  rowReorder: {
+                      selector: 'td:nth-child(2)'
+                  },
                   dom: 'Bfrtip',
               buttons: [
                   'copy', 'excel', 'pdf', 'csv'
